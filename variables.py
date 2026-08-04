913eab56f69236d8e918bd1e254d8c4a39afae83
Victor Fattor <victor.fattor@bosonic.digital>
2026-08-04 08:33:56 -0300

    curso python: clase variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+"""
+VARIABLE CREATION
+
+PYTHON ES UN LENGUAJE DE TIPADO DINAMICO, NO ES FUERTEMENTE TIPADO
+
+<variable name> = <variable value>
+"""
+
+name = "Victor Fattor" # Variable de tipo string
+
+age = 47
+print(name)
+
+# para conocer el tipo de variables utilizamos la función type()
+
+tipo = type(name)
+tipo2 = type(age)
+print(tipo)
+print(tipo2)
+
+"""
+hay una convención en python y es que 
+en los nombres de las variables
+se utiliza snake_case
+
+ejemplo:   nombre_objeto = "monitor"
+"""
